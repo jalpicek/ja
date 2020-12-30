9a5396d75f1acb0780e03bbaedb74b13aae811bd
jalpicek <76773613+jalpicek@users.noreply.github.com>
2020-12-30 13:04:09 +0100

    Update podpory v2.js

--- podpory v2.js
+++ podpory v2.js
@@ -256,7 +256,7 @@ function calculateEverything() {
                             }
                         })
 						console.log("Zde chci pocet utoku ");
-						console.log(rows.eq(rowNr).children()[13].text().trim());
+						console.log(rows.eq(rowNr).children());
 						villageData[thisID]["attacks"] = 55;
                     });
 
